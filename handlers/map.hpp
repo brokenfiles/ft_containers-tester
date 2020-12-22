bf2c454fe49093a0720061ad81ea2dd12802d64a
brokenfiles <louis.nevysup@gmail.com>
2020-12-22 19:07:01 +0100

    First commit

---
+++ handlers/map.hpp
@@ -0,0 +1,27 @@
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include "../Tester.hpp"
+#include "../unit_tests/map_tests.hpp"
+
+template <class Map>
+void exec_test_map(wanted_tests_type tests = wanted_tests_type())
+{
+	new_type("Map");
+	{
+		//register tests
+		register_test("insert_and_change_values", insert_and_change_values<Map>);
+		register_test("constructors", constructors<Map>);
+		register_test("insert_test", insert_test<Map>);
+		register_test("operators", operators<Map>);
+		register_test("iterators_tests", iterators_tests<Map>);
+		register_test("capacity_tests", capacity_tests<Map>);
+		register_test("erase_test", erase_test<Map>);
+		register_test("swap_test", swap_test<Map>);
+		register_test("find_tests", find_tests<Map>);
+		register_test("bounds_tests", bounds_tests<Map>);
+	}
+	//launch tests
+	global.start_test(tests);
+}
+#endif
